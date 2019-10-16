19df8ffccbc6beb7537c3499b87e565e122986a5
Can Elmas <Can.Elmas@monitise.com>
2019-10-16 14:20:21 +0300

    Add ability to pass device properties as env vars

--- data-producer/generators/device_generator.js
+++ data-producer/generators/device_generator.js
@@ -29,27 +29,27 @@ let ANDROID_MODELS = [
 
 let generate = (deviceId) => {
 
-  let platform = _.sample(["ANDROID", "iOS"])
-  let brand = getDeviceBrand(platform)
-  let model = getDeviceModel(brand)
+  let platform = _.sample(["ANDROID", "iOS"]) || process.env.DP_PLATFORM
+  let brand = getDeviceBrand(platform) || process.env.DP_BRAND
+  let model = getDeviceModel(brand) || process.env.DP_MODEL
 
   return {
     deviceId: deviceId,
-    language: _.sample(["tr", "en", "fr", "it", "es", "hy", "eu", "bg", "la", "pl", "uk"]),
-    country: _.sample(["US", "TR", "ES", "PT", "PL", "UA", "NO", "MT", "KR", "IT", "DE", "PY", "PE", "QA", "SN", "ZA", "CH", "AL", "AO", "BS", "AU", "BG", "CA", "DK", "FR", "IN", "JP", "MY", "MX", "GB", "ZW"]),
-    appVersionName: _.sample(["1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5", "2.0.0", "2.1.0", "2.2.2"]),
-    appVersionCode: _.sample(_.range(1, 20)),
-    appPackageName: "com.lovely.app",
-    sdkVersion: _.sample(["1.2.10", "1.3.0", "1.1.0", "1.1.1", "2.0.0", "2.1.0", "2.2.0", "2.2.1"]),
+    language: _.sample(["tr", "en", "fr", "it", "es", "hy", "eu", "bg", "la", "pl", "uk"]) || process.env.DP_LANGUAGE,
+    country: _.sample(["US", "TR", "ES", "PT", "PL", "UA", "NO", "MT", "KR", "IT", "DE", "PY", "PE", "QA", "SN", "ZA", "CH", "AL", "AO", "BS", "AU", "BG", "CA", "DK", "FR", "IN", "JP", "MY", "MX", "GB", "ZW"]) || process.env.DP_COUNTRY,
+    appVersionName: _.sample(["1.0.0", "1.0.1", "1.0.2", "1.0.3", "1.0.4", "1.0.5", "2.0.0", "2.1.0", "2.2.2"]) || process.env.DP_APP_VERSION_NAME,
+    appVersionCode: _.sample(_.range(1, 20)) || process.env.DP_APP_VERSION_CODE,
+    appPackageName: "com.lovely.app" || process.env.DP_APP_PACKAGE_NAME,
+    sdkVersion: _.sample(["1.2.10", "1.3.0", "1.1.0", "1.1.1", "2.0.0", "2.1.0", "2.2.0", "2.2.1"]) || process.env.DP_SDK_VERSION,
     timezone: _.sample(["Europe/Minsk", "Europe/Brussels", "America/Sao_Paulo", "Europe/Berlin", "Europe/Rome", "Europe/Moscow", "Europe/Istanbul", "Europe/London",
       "America/Chicago", "America/Los_Angeles", "America/New_York", "Europe/Vatican", "Asia/Seoul", "Europe/Madrid"
-    ]),
+    ]) || process.env.DP_TIMEZONE,
     platform: platform,
-    deviceCategory: _.sample(["PHONE", "DESKTOP"]),
+    deviceCategory: _.sample(["PHONE", "DESKTOP"]) || process.env.DP_DEVICE_CATEGORY,
     deviceBrand: brand,
     deviceModel: model,
-    osVersion: _.sample(["9", "24", "12.0.1", "10.13", "7.1.1", "11.4.1", "16", "12.1.0", "6.0", "10.1", "11.1.1"]),
-    carrier: _.sample(["Vodafone", "Turkcell", "Unknown", "Verizon", "Sprint", "Turk Telekom", "Bell", "AT&T"])
+    osVersion: _.sample(["9", "24", "12.0.1", "10.13", "7.1.1", "11.4.1", "16", "12.1.0", "6.0", "10.1", "11.1.1"]) || process.env.DP_OS_VERSION,
+    carrier: _.sample(["Vodafone", "Turkcell", "Unknown", "Verizon", "Sprint", "Turk Telekom", "Bell", "AT&T"]) || process.env.DP_CARRIER
   }
 
 }
